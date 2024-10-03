9bde8836b5794c97aebeeed09fd371a98aa455c8
narengithub009 <91239876+narengithub009@users.noreply.github.com>
2024-10-03 16:35:26 +0000

    changes done

---
+++ temp/test/check_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+check_file(){
+	
+	if [ -f "$1" ]; then
+		echo "File exists "
+		return 0
+
+	else
+		echo "File doesn't exists"
+		return 1
+	fi
+
+}
+
+check_file "/workspaces/copilotcodespace/temp/test/file1.sh"
+
