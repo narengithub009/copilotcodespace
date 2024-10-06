f9790215134e6786b9e7aa8fbdbdbac9b4b10710
narengithub009 <91239876+narengithub009@users.noreply.github.com>
2024-10-06 01:01:24 +0000

    changes done

---
+++ temp/test/file9.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# check if directory provided as argument 
+
+if [ -z "$1" ]; then
+	echo "Usage $0 <>directory"
+	echo "$1"
+       exit 1
+fi
+
+DIR=/workspaces/copilotcodespace/temp/test
+
+if [ -d "DIR" ]; then
+	echo "Error: $DIR is not a valid directory."
+	exit 1
+fi	
+
+file_count=$(ls -l "$DIR" | grep "^-" | wc -l)
+echo "Number of files in directory '$DIR': $file_count"
+
+
+echo "files and their permissions :"
+ls -l "$DIR" | grep "^-"| awk '{print $1, $9}'
+
